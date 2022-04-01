0011a7590a8788f6deafb042476d41298c20a089
kurasikirwa <97512594+kurasikirwa@users.noreply.github.com>
2022-04-02 02:09:20 +0500

    Шаг 1

--- INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
+++ INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
@@ -1,1 +1,31 @@
-﻿
+﻿#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/freeglut.h>
+
+#include <iostream>
+
+
+static void RenderSceneCB()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(1024, 768);
+	glutCreateWindow("IJGRAF_LabWork1");
+
+	glutDisplayFunc(RenderSceneCB);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	glutMainLoop();
+
+	return 0;
+}

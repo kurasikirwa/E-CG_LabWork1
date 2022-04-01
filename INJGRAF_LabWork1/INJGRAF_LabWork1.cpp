ce0bebe3f6c67c35fe23ad6770761c7879cbfeb7
kurasikirwa <97512594+kurasikirwa@users.noreply.github.com>
2022-04-02 02:21:38 +0500

    Шаг 2

--- INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
+++ INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
@@ -1,14 +1,33 @@
 ﻿#include <GL/glew.h>
-#include <GL/GL.h>
 #include <GL/freeglut.h>
 
-#include <iostream>
+#include <glm/vec3.hpp>
 
+GLuint VBO;
 
 static void RenderSceneCB()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_POINTS, 0, 1);
+
+	glDisableVertexAttribArray(0);
+
 	glutSwapBuffers();
+}
+
+static void CreateVertexBuffer()
+{
+	glm::vec3 Vertices[1];
+	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
 }
 
 int main(int argc, char** argv)
@@ -24,6 +43,14 @@ int main(int argc, char** argv)
 	glutDisplayFunc(RenderSceneCB);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK) {
+		fprintf(stderr, "Error: %s'\n", glewGetErrorString(res));
+		return 1;
+	}
+
+	CreateVertexBuffer();
 
 	glutMainLoop();
 

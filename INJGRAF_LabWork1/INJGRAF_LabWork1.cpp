3670772d12779bf1b0890c451127cf5faf517aeb
kurasikirwa <97512594+kurasikirwa@users.noreply.github.com>
2022-04-02 02:28:32 +0500

    Шаг 3

--- INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
+++ INJGRAF_LabWork1/INJGRAF_LabWork1.cpp
@@ -13,7 +13,7 @@ static void RenderSceneCB()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glDisableVertexAttribArray(0);
 
@@ -22,8 +22,10 @@ static void RenderSceneCB()
 
 static void CreateVertexBuffer()
 {
-	glm::vec3 Vertices[1];
-	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
 
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
